60231ada278019749eb14cd93ca725d73994c811
GFFritz <gabrielf.fritz@gmail.com>
2024-01-14 12:47:38 -0300

    Center block

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={
+          `max-w-screen max-h-screen overflow-hidden ` + inter.className
+        }
+      >
+        {children}
+      </body>
     </html>
   )
 }
